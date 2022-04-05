fec677f71248ae73cfd412a2b6ce25c1d2cfe3ca
SuperCoder <supercoder@AirvonSperCoder.fritz.box>
2022-04-05 17:40:58 +0200

    added little style

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require("express")
 const PORT = 2304
 const bodyParser = require('body-parser')
 const { body, validationResult } = require('express-validator')
+const fs = require("fs")
+const path = require("path")
+const users = require('./userlist.json')
+    // const config = require("./config.json")
 
 
 // npm install express ejs body-parser express-validator
@@ -10,10 +14,8 @@ const { body, validationResult } = require('express-validator')
 const app = express()
 app.set("view engine", "ejs")
 
-const users = []
-
 app.use(bodyParser.urlencoded())
-console.log(bodyParser);
+    // console.log(bodyParser);
 
 app.use((req, _, next) => {
     console.log("New Request", req.method, req.url);
@@ -26,6 +28,15 @@ app.get("/", (_, res) => {
     res.render("home", { users })
 })
 
+// fs.readFile("./data.json", 'utf-8', (err, data) =>{
+//     if(err){
+//         console.log("Error in readfile error.son");
+//     }
+//     else {
+//         users = JSON.parse(data)
+//     }
+// })
+
 app.post(
     "/neueUser",
     body("name").isLength({ min: 1, max: 20 }),
@@ -34,7 +45,7 @@ app.post(
     body("message").isLength({ min: 1, max: 100 }),
     (req, res) => {
         const neuerUser = req.body
-
+        console.log(neuerUser);
         const errors = validationResult(req)
         const isValidUser = errors.isEmpty()
 
@@ -44,11 +55,10 @@ app.post(
                 description: "user input is invalid, please try again...",
                 validationErrors
             })
-            console.log(validationErrors);
             return
         }
 
-        const userExistsAlready = users.find(user => user.username === neuerUser.username)
+        const userExistsAlready = users.find(user => user.nachname === neuerUser.nachname)
         if (userExistsAlready) {
             res.render("invalidInputError", {
                 description: "user already exists",
@@ -57,13 +67,32 @@ app.post(
             return
         }
 
+        // fs.readFile("./config.json", "utf-8", (err, jsonString) => {
+        //     if (err) {
+        //         console.log("File read failed:", err);
+        //         return
+        //     }
+        //     try {
+        //         users = JSON.parse(jsonString)
+        //         console.log("Costumoer address is:", users);
+        //     } catch (err) {
+        //         console.log("Error parsting JSOn String:", err);
+        //     }
+        // })
+
         users.push(neuerUser)
+
+        fs.writeFile('./userlist.json', JSON.stringify(users), (err) => {
+            if (err) {
+                console.log('error wring json', err);
+                return
+            }
+        })
+
         res.redirect("/")
+
     }
 )
-
-
-
 
 
 
